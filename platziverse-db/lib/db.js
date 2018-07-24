d671d1bed5a96c26558fd6e40310426ab2bf1eeb
Fabian Gutierrez <fega.hg@gmail.com>
2018-07-24 14:43:05 -0500

    test estan fallando

---
+++ platziverse-db/lib/db.js
@@ -0,0 +1,11 @@
+'usa strict'
+
+const Sequelize = require('sequelize')
+let sequelize = null
+
+module.exports = function setupDatabase (config) {
+  if (!sequelize) {
+    sequelize = new Sequelize(config)
+  }
+  return sequelize
+}
